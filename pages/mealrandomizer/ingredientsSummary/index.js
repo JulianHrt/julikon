8a4c89c13e3027bb9fcc3f62604a6f3f17092d69
Julian Hirt <julian.hirt94@web.de>
2023-01-17 21:05:32 +0100

    change typo

--- pages/mealrandomizer/ingredientsSummary/index.js
+++ pages/mealrandomizer/ingredientsSummary/index.js
@@ -1,17 +1,13 @@
 export default function IngredientsSummary({ isMeals }) {
   const meals = isMeals;
 
-  if (meals.length >= 0) {
-    return <p>keine Gerichte - Keine Zutaten</p>;
-  } else {
-    return (
-      <ul>
-        {meals.map((meal) => {
-          return meal.ingredients.map((tag) => {
-            return <li key={tag}>{tag}</li>;
-          });
-        })}
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      {meals.map((meal) => {
+        return meal.ingredients.map((tag) => {
+          return <li key={tag}>{tag}</li>;
+        });
+      })}
+    </ul>
+  );
 }
