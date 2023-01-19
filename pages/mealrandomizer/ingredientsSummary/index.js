3fba5801994bd3cbfe5cad9e92fc3bb8f78177cf
Julian Hirt <julian.hirt94@web.de>
2023-01-19 18:11:37 +0100

    added checkbox to ingredients

--- pages/mealrandomizer/ingredientsSummary/index.js
+++ pages/mealrandomizer/ingredientsSummary/index.js
@@ -1,13 +1,64 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { nanoid } from "nanoid";
+import styled from "styled-components";
+
 export default function IngredientsSummary({ isMeals }) {
-  const meals = isMeals;
+  const [isIngredients, setIngredients] = useState([]);
+
+  const ingredients = isMeals.map((meal) => {
+    return meal.ingredients.map((ingredient) => {
+      return { ingredient, bought: false, id: nanoid() };
+    });
+  });
+
+  function setTrue(key) {
+    const newArray = isIngredients.map((one) => {
+      return one.map((eins) => {
+        if (eins.id === key) {
+          return { ...eins, bought: true };
+        } else {
+          return { ...eins };
+        }
+      });
+    });
+    setIngredients(newArray);
+  }
+  useEffect(() => {
+    setIngredients(ingredients);
+  }, isMeals);
 
   return (
     <ul>
-      {meals.map((meal) => {
-        return meal.ingredients.map((tag) => {
-          return <li key={tag}>{tag}</li>;
+      {isIngredients.map((one) => {
+        return one.map((eins) => {
+          if (eins.bought === false) {
+            return (
+              <StyledList key={eins.id}>
+                <StyledCheckBox
+                  type="checkbox"
+                  onChange={() => setTrue(eins.id)}
+                ></StyledCheckBox>
+                {eins.ingredient}
+              </StyledList>
+            );
+          }
         });
       })}
     </ul>
   );
 }
+
+const StyledList = styled.li`
+  list-style: none;
+  width: 90vw;
+  margin: 1rem;
+  background-color: lightgray;
+  font-size: 2rem;
+  font-weight: bold;
+  border-radius: 10px;
+`;
+
+const StyledCheckBox = styled.input`
+  margin: 0.5rem;
+`;
